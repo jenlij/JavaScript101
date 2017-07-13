5ac6487f77bdc567c19523b5bcec48d7e7611a87
jenlij <jljohns1216@gmail.com>
2017-07-13 10:17:45 -0400

    writes part one of exercises102

--- scripts/main.js
+++ scripts/main.js
@@ -59,7 +59,7 @@ console.log(splitAmount(40, 'fair', 2));
 
 //102 exercises
 function printNumbers(start, end) {
-    for (var i = start; i <= end; i ++) {
+    for (var i = start; i <= end; i++) {
         console.log(i + "\n");
     }    
 }
@@ -76,15 +76,95 @@ function printNumbers2(start, end) {
 printNumbers2(1, 10);
 
 //print square
-function printSquare() {
-    
+function printSquare(size) {
+    for (var i = 0; i < size; i++) {
+        console.log('*'.repeat(size));
+    }
 }
+printSquare(5);
+console.log('_'.repeat(20));
+
 //print box
+function printBox(width, height) {
+    var section = width - 1;
+    var space = ' ';
+    if ((height > 2) && (width >=2)) {
+        var str = new Array(section).join(space);
+        console.log('*'.repeat(width));
+        for (var i = 0; i < (height - 2); i++) {
+            console.log('*'+ str +"*")
+        }
+        console.log('*'.repeat(width));
+    }
+    else if (height == 2) {
+        console.log('*'.repeat(width));
+        console.log('*'.repeat(width));
+    }
+    else if (height == 1) {
+        console.log('*'.repeat(width));
+    }
+    else if ((height > 2) && (width == 1)) {
+        for (var i = 0; i < height; i++) {
+            console.log('*');
+        }
+    }
+    else {
+        console.log('no box');
+    }
+}
+ 
+printBox(6, 4);
+console.log('_'.repeat(20));
+printBox(4, 2);
+console.log('_'.repeat(20));
+printBox(3, 1);
+console.log('_'.repeat(20));
+printBox(1, 3);
+console.log('_'.repeat(20));
+printBox(0,0);
+console.log('_'.repeat(20));
+
 
 //print banner
+function printBanner(str) {
+    console.log('*'.repeat(str.length + 4));
+    console.log('* ' + str + ' *');
+    console.log('*'.repeat(str.length + 4));
+}
+
+printBanner('Welcome to DigitalCrafts');
+
 
 //factor a number
+function factorMe(number) {
+    var i = 1;
+    var factors = new Array();
+    while (i <= number) {
+        if (number % i == 0) {
+            factors.push(i);
+        }
+        i++;
+    }
+    console.log(factors);
+}
+factorMe(120);     
 
 //caesar cipher
 
 //caesar cipher 2
+
+//leetspeak
+
+//long long vowels
+
+//sum the nums
+
+//just positives
+
+//matrix addition
+
+//matrix multiplication
+
+//rock paper scissors
+
+//tic tac toe
